b7fb9c0f22045a2f00f79fdfdcccce915cf6fa1b
Bjoern Petri <bpetri@gmx.net>
2016-06-02 10:36:28 +0200

    Another test

--- test.groovy
+++ test.groovy
@@ -3,7 +3,7 @@
 def deploy(componentname) {
   stage 'Deploy ' + componentname
   sh 'echo  ' + componentname + ' release'
-  withEnv(["TEST_STR=ThisisJob${env.JOBNAME}andComponent" + component]) {
+  withEnv(["TEST_STR=ThisisJob${env.JOBNAME}andComponent" + componentname]) {
     sh 'echo hallo'
     sh 'echo $TEST_STR'
   }
